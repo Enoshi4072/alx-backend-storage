f50a5c326fbc1a7fa922e99e69fc2564318e1afb
Enoshi4072 <enockkibet1877@gmail.com>
2024-03-31 06:44:17 +0000

    Procedure fot computing Weighted average for all students

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,19 @@
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    -- Clear existing average scores
+    UPDATE users SET average_score = 0;
+
+    -- Calculate and update average scores for each user
+    UPDATE users
+    JOIN (
+        SELECT c.user_id, SUM(c.score * p.weight) / SUM(p.weight) AS average_weighted_score
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        GROUP BY c.user_id
+    ) AS temp ON users.id = temp.user_id
+    SET users.average_score = temp.average_weighted_score;
+END //
+
+DELIMITER ;
